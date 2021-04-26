10745fee8deddd05848252e1fcd7a4348bb8115c
eliav2 <louski.a@gmail.com>
2021-04-26 13:22:53 +0300

    'message'

---
+++ README.md
@@ -0,0 +1,41 @@
+# react-use-call-onnext-render
+
+`useCallOnNextRender` - an efficient and smart hook to schedule a callback for later render.
+
+## installation
+
+with npm `npm react-use-call-onnext-render`.  
+or `yarn add react-use-call-onnext-render`
+
+## Examples
+
+#### Demos
+
+code sandbox:
+[//]: # (todo: add code sandbox)
+
+### examples:
+
+**schedule a callback**:
+this will behave differenty from non force call.
+
+```jsx
+```
+
+**schedule a callback and force re-render**:
+
+```jsx
+```
+
+### Properties
+
+here properties description.
+
+this repo also includes:  
+`useCallOnNextIteration` - like a managed queue for of calls. see examples below.
+
+## useCallOnNextIteration
+
+## Versions
+
+See [CHANGELOG.md](https://github.com/Eliav2/react-xarrows/blob/master/CHANGELOG.md) in this repo.
---
+++ src/index.ts
@@ -0,0 +1,69 @@
+import React, {useEffect, useReducer, useRef} from 'react';
+
+// will run 'func' on the callCount nth call of the callFunc.current function
+export const useCallOnNextIteration = () => {
+    const _callCount = useRef(0);
+    const callIn = useRef({});
+    const callFunc = useRef((func, callCount = 1) => {
+        const next = _callCount.current + callCount;
+        if (!(next in callIn.current)) callIn.current[next] = [];
+        // schedule the func to be called in the 'next' nth call
+        callIn.current[next].unshift(func);
+        // call any functions scheduled for now
+        if (_callCount.current in callIn.current) {
+            callIn.current[_callCount.current].forEach((func) => func());
+            delete callIn[_callCount.current];
+        }
+        _callCount.current += 1;
+    });
+    return callFunc.current;
+};
+//// example usage
+// const callOnNextIteration = useCallOnNextIteration();
+// for (let i = 0; i < 10; i++) {
+//   console.log('i=', i);
+//   callOnNextIteration(() => console.log('callOnNextIteration call', i), 5);
+// }
+
+
+type renderQueue = { [renderCount: number]: { callback: Function; forceRender: boolean }[] };
+
+// will run 'func' on the renderCount nth render of the renderFunc.current function
+export const useCallOnNextRender = (deps?) => {
+    //for force update logic
+    const [, forceUpdate] = useReducer((x) => x + 1, 0);
+    //for next renders logic
+    const _renderCount = useRef(0);
+    const renderIn = useRef<renderQueue>({});
+    const renderFunc = useRef((func, renderCount = 1, forceRender = false) => {
+        const next = _renderCount.current + renderCount;
+        if (!(next in renderIn.current)) renderIn.current[next] = [];
+        // schedule the func to be rendered in the 'next' nth render
+        renderIn.current[next].unshift({callback: func, forceRender});
+    });
+    useEffect(() => {
+        // force update if requested
+        for (const [renderCount, scheduledCalls] of Object.entries(renderIn.current)) {
+            // if one of the scheduled calls are forced then force re-render to the wanted renderCount
+            let breakFlag = false;
+            for (const call of scheduledCalls) {
+                if (call.forceRender && (Number(renderCount) > _renderCount.current ?? true)) {
+                    forceUpdate();
+                    breakFlag = true;
+                    break;
+                }
+            }
+            if (breakFlag) break;
+        }
+        // call any functions scheduled for this render
+        if (_renderCount.current in renderIn.current) {
+            const currentCalls = renderIn.current[_renderCount.current];
+            currentCalls.forEach((call) => call.callback());
+            delete renderIn[_renderCount.current];
+        }
+
+        //update render count
+        _renderCount.current += 1;
+    }, deps);
+    return renderFunc.current;
+};
